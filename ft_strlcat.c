012bf26d649d8f81aa2ecb0fb2616dbfec31e48f
Pjotr Van dijk <pvan-dij@f1r3s15.codam.nl>
2021-10-07 16:03:46 +0200

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,22 +3,22 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	j;
 	size_t	dstlen;
 	size_t	srclen;
 
 	dstlen = ft_strlen(dst);
 	srclen = ft_strlen(src);
-	i = dstlen;
-	j = 0;
-	if (size < 1)
-		return (srclen + size);
-	while (src[j] && i < size - 1)
+	i = 0;
+	if (size <= dstlen)
+		return (size + srclen);
+	if (size >= dstlen)
 	{
-		dst[i++] = src[j++];
+		while (src[i] && dstlen + i + 1 < size)
+		{
+			dst[dstlen + i] = src[i];
+			i++;
+		}
 	}
-	dst[i] = '\0';
-	if (size > dstlen)
-		return (dstlen + srclen);
-	return (size + srclen);
+	dst[dstlen + i] = 0;
+	return (dstlen + srclen);
 }
