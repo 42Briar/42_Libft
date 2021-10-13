eaca3571594a950cb115cd756904a07b32cfbd81
Pjotr Van dijk <pvan-dij@f1r3s1.codam.nl>
2021-10-13 13:36:35 +0200

    fixes

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: pvan-dij <pvan-dij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/08 12:18:51 by pvan-dij      #+#    #+#                 */
-/*   Updated: 2021/10/08 12:21:52 by pvan-dij      ########   odam.nl         */
+/*   Updated: 2021/10/13 12:17:39 by pvan-dij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,5 @@ void	ft_putstr_fd(char *s, int fd)
 
 	i = 0;
 	while (s[i])
-	{
 		write(fd, &s[i++], 1);
-	}
 }
